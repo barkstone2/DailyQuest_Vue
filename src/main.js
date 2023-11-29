08cc0c670d580825ac4b00f7c6f9bf3d855e8b7e
barkstone <barkstone2@gmail.com>
2023-11-29 14:40:42 +0900

    feat: 라우터 인터셉터에 관리자 메뉴와 사이드 메뉴 처리 로직 추가

--- src/main.js
+++ src/main.js
@@ -9,6 +9,8 @@ import '@/axios/index'
 import '@mdi/font/css/materialdesignicons.css'
 
 import { principal } from '@/stores/principal'
+import { menu } from '@/stores/menu'
+
 if (!principal.id) {
   principal.synchronize()
 }
@@ -24,12 +26,22 @@ router.beforeEach((to, from) => {
     return router.go(-1)
   }
 
+  // 관리자 페이지 접근 시 권한이 없는 경우
+  if (!principal.isAdmin && to.meta.needAdmin) {
+    alert('권한이 없습니다.')
+    return router.go(-1)
+  }
+
   // 로그인 되지 않은 상태로 권한이 필요한 페이지에 접근하는 경우
   if (!principal.id && to.meta.needAuthentication) {
     menu.rememberRedirectRoute(to.path)
     return { path: '/login' }
   }
 
+  // 접근한 페이지가 사이드 메뉴에 존재하는 경우
+  if (to.meta.group) {
+    menu.selectedMenuOnSide = to.meta.group
+  }
 })
 
 router.onError((error) => {
