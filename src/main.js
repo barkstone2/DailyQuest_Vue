32322d4bcb733605288a8cbcf7be75271be9a710
barkstone <barkstone2@gmail.com>
2023-11-29 14:22:29 +0900

    feat: 피니아 코드 제거

--- src/main.js
+++ src/main.js
@@ -1,15 +1,13 @@
 import { createApp } from 'vue'
-import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
 import vuetify from './vuetify'
 
-import './assets/main.css'
+import '@/assets/main.css'
 
 const app = createApp(App)
 
-app.use(createPinia())
 app.use(router)
 app.use(vuetify)
 
