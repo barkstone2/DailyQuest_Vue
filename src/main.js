98b00a38d90407098266f05e23c7514e573a236c
barkstone <barkstone2@gmail.com>
2023-11-29 14:36:33 +0900

    feat: 라우터 인터셉터 추가

--- src/main.js
+++ src/main.js
@@ -5,6 +5,38 @@ import router from './router'
 import vuetify from './vuetify'
 
 import '@/assets/main.css'
+import '@/axios/index'
+import '@mdi/font/css/materialdesignicons.css'
+
+import { principal } from '@/stores/principal'
+if (!principal.id) {
+  principal.synchronize()
+}
+
+router.beforeEach((to, from) => {
+  // 로그인 페이지에서 다시 로그인 페이지로 이동하는 경우
+  if (from.path === '/login' && to.path === '/login') {
+    return false
+  }
+
+  // 로그인된 상태로 로그인 페이지로 이동하는 경우
+  if (principal.id && to.path === '/login') {
+    return router.go(-1)
+  }
+
+  // 로그인 되지 않은 상태로 권한이 필요한 페이지에 접근하는 경우
+  if (!principal.id && to.meta.needAuthentication) {
+    menu.rememberRedirectRoute(to.path)
+    return { path: '/login' }
+  }
+
+})
+
+router.onError((error) => {
+  if(error.errorCode === 401) {
+    return { path : '/login' }
+  }
+})
 
 const app = createApp(App)
 
