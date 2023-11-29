a522677bb8fe2d2d632ef8d2cdc7d204c6ef6d6d
barkstone <barkstone2@gmail.com>
2023-11-29 14:29:19 +0900

    feat: vuetify md3 설정 추가

--- src/vuetify/index.js
+++ src/vuetify/index.js
@@ -1,12 +1,11 @@
 // Vuetify
 import 'vuetify/styles'
 import { createVuetify } from 'vuetify'
-import * as components from 'vuetify/components'
-import * as directives from 'vuetify/directives'
+
+import { md3 } from 'vuetify/blueprints'
 
 const vuetify = createVuetify({
-  components,
-  directives,
+  blueprint: md3
 })
 
 export default vuetify
