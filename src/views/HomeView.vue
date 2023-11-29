1e5a8f709ea599204cb326129a769eaa58135889
barkstone <barkstone2@gmail.com>
2023-11-29 14:27:17 +0900

    feat: 기본 Vue 컴포넌트 추가
    - NotFoundView 재작성

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,7 @@
 <script setup>
-import TheWelcome from '../components/TheWelcome.vue'
+import router from '@/router';
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+    <span></span>
 </template>
