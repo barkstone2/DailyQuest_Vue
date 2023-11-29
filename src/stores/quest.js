8c4c55636760dce57c14d6e8aeb3a7d1282910d3
barkstone <barkstone2@gmail.com>
2023-11-29 14:41:17 +0900

    feat: 퀘스트 스토어 파일 추가

---
+++ src/stores/quest.js
@@ -0,0 +1,29 @@
+import { reactive } from 'vue'
+export const dto = reactive({
+  id: null,
+  title: '',
+  description: '',
+  deadLine: '',
+  details: [],
+  reset() {
+    this.id = null
+    this.title = ''
+    this.description = ''
+    this.deadLine = ''
+    this.details = []
+  },
+  set(quest) {
+    this.id = quest.id
+    this.title = quest.title
+    this.description = quest.description
+    this.deadLine = quest.deadLine
+    this.details = quest.detailQuests.map((detail) => {
+      return {
+        title: detail.title,
+        description: detail.description,
+        type: detail.type,
+        targetCount: detail.targetCount
+      }
+    })
+  }
+})
