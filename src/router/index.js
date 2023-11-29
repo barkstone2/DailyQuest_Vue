00035f85c59843fb6f281ab9d2d0edf7d581655c
barkstone <barkstone2@gmail.com>
2023-11-29 12:02:49 +0900

    feat: 기본 라우터 정보 추가

--- src/router/index.js
+++ src/router/index.js
@@ -2,28 +2,28 @@ import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import NotFound from '../views/NotFound.vue'
 
+const routes = [
+  {
+    path: '/',
+    name: 'home',
+    component: () => import('../views/HomeView.vue'),
+    meat: { needAuthentication: true }
+  },
+  {
+    path: '/:pathMatches(.*)',
+    name: 'notFound',
+    component: () => import('../views/NotFoundView.vue')
+  },
+  {
+    path: '/login',
+    name: 'login',
+    component: () => import('../views/LoginView.vue')
+  },
+]
+
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: HomeView
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
-    },
-    {
-      path: '/test-:pathMatches(.*)',
-      name: 'notFound',
-      component: NotFound
-    }
-  ]
+  routes: routes
 })
 
 export default router
