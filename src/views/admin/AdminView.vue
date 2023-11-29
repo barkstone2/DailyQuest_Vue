8a7adfb1620dc4a03e607b4c420d2319c0ef8d71
barkstone <barkstone2@gmail.com>
2023-11-29 14:54:08 +0900

    feat: 관리자 뷰 및 라우팅 정보 추가

---
+++ src/views/admin/AdminView.vue
@@ -0,0 +1,31 @@
+<script setup>
+import { ref } from 'vue';
+import { admin } from '@/stores/admin';
+
+const tab = ref(null)
+admin.getSettings()
+admin.getExpTable()
+
+</script>
+
+<template>
+    <VContainer class="w-75 h-75">
+        <div class="d-flex justify-center">
+            <VTabs bg-color="white" color="black" v-model="tab" hide-slider align-tabs="center" selected-class="selected-tab" class="ma-2 w-fit rounded-lg">
+                <RouterLink to="/admin" style="color: black;">
+                    <VTab value="system">시스템 설정</VTab>
+                </RouterLink>
+                <RouterLink to="/admin/exp-table" style="color: black;">
+                    <VTab value="exp-table">경험치 테이블 관리</VTab>
+                </RouterLink>
+            </VTabs>
+        </div>
+        <RouterView></RouterView>
+    </VContainer>
+</template>
+
+<style scoped>
+.selected-tab {
+    background-color: lightgray;
+}
+</style>
