3489f83ef00497ac48ea7fd26375ebd9ffbd5520
barkstone <barkstone2@gmail.com>
2024-04-25 14:01:23 +0900

    feat: Axios 오류 메시지 표시 기능 추가 및 리팩터링

--- src/axios/index.js
+++ src/axios/index.js
@@ -6,55 +6,71 @@ import { PRINCIPAL } from '@/stores/principal'
 axios.defaults.withCredentials = true
 axios.defaults.baseURL = API_CONFIG.SERVER_URL
 
+const isCommandRequest = (config) => { return config.method.toLowerCase() === ('post' || 'patch' || 'put' || 'delete') }
+const createRequestKey = (config) => { return `${config.method} ${config.url}` }
+
 const pendingRequests = new Set();
 axios.interceptors.request.use(config => {
-  let method = config.method;
-  if(method !== ('get' || 'GET')) {
-    let requestKey = `${method} ${config.url}`
-    if(pendingRequests.has(requestKey)) {
+  if (isCommandRequest(config)) {
+    const requestKey = createRequestKey(config)
+    if (pendingRequests.has(requestKey)) {
       return Promise.reject(new Cancel())
     }
     pendingRequests.add(requestKey)
   }
   return config
-})
-axios.interceptors.response.use(
-  (response) => {
-    if(response.config.method !== ('get' || 'GET')) {
-      pendingRequests.delete(`${response.config.method} ${response.config.url}`);
-    }
-    return response
-  },
-  (error) => {
-    // server error 핸들링
-    if(error.code === 'ERR_NETWORK') {
-      API_CONFIG.SERVER_ERROR = true
-      PRINCIPAL.invalidate()
-      router.push('/error')
-      return
-    }
+});
 
-    API_CONFIG.SERVER_ERROR = false
-
-    if(axios.isCancel(error)) {
-      alert('요청 처리 중입니다. 잠시 후 다시 시도해주세요.')
-      return
-    }
-
-    pendingRequests.delete(`${error.config.method} ${error.config.url}`)
-
-    // Principal sync 처리 시에 발생하는 오류는 무시
-    if (error.config.url === '/users') return
-    const state = error.response.status
-
-    // 토큰이 만료 됐을 때의 처리
-    switch (state) {
-      case 401:
-        PRINCIPAL.invalidate()
-        router.push('/login')
-        break
-      default:
-        return Promise.reject(error)
-    }
+const responseInterceptor = (response) => {
+  const config = response.config;
+  if(isCommandRequest(config)) {
+    const requestKey = createRequestKey(config)
+    pendingRequests.delete(requestKey);
   }
+  return response
+};
+
+const errorInterceptor = (error) => {
+  const isServerError = (error) => { return error.code === 'ERR_NETWORK' }
+  if(isServerError(error)) {
+    API_CONFIG.SERVER_ERROR = true
+    PRINCIPAL.invalidate()
+    router.push('/error')
+    return
+  }
+  API_CONFIG.SERVER_ERROR = false
+  
+  if(axios.isCancel(error)) {
+    alert('요청 처리 중입니다. 잠시 후 다시 시도해주세요.')
+    return
+  }
+  
+  const config = error.config
+  const requestKey = createRequestKey(config)
+  pendingRequests.delete(requestKey)
+  
+  // Principal sync 처리 시에 발생하는 오류는 무시
+  const isUserSync = (config) => { return config.method.toLowerCase() === 'get' && config.url === '/users' }
+  if (isUserSync(config)) {
+    return
+  }
+  
+  const response = error.response
+  const state = response.status
+  const data = response.data
+  const message = data?.errorResponse?.message
+  alert(message)
+  
+  switch (state) {
+    case 401:
+      PRINCIPAL.invalidate()
+      router.push('/login')
+      break
+  }
+  return Promise.reject(message)
+};
+
+axios.interceptors.response.use(
+  responseInterceptor,
+  errorInterceptor
 )
